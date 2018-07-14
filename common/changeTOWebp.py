fbb066a01c18ea91bcf0c65f5561a4026f5f5533
libp <924701358@qq.com>
2018-07-14 18:07:13 +0800

    将图片裁剪并压缩成webp格式，原图113kb，压缩后13kb

--- common/changeTOWebp.py
+++ common/changeTOWebp.py
@@ -1,23 +1,49 @@
 # -*- coding: utf-8 -*-
-import glob
+from glob import glob
 import os
-import threading
+from  threading import Thread
 
+import logging
 from PIL import Image
 
 
-def create_image(infile, index):
-  os.path.splitext(infile)
-  im = Image.open(infile)
-  print infile
-  print index
-  im.save("wepy/webp_" + str(index) + ".webp", "WEBP")
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=
+    '%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+    datefmt='%a, %d %b %Y %H:%M:%S',
+    )
 
+def convert_img_type(infile, index):
+
+  encoder_path = "C:/Users/ruonan/libwebp-1.0.0-windows-x64/bin/cwebp.exe"
+  output_path = "C:/Test01/20150806205033/webp/"
+  if not os.path.exists(output_path):
+    os.mkdir(output_path)
+  new_size = get_new_size(infile)
+  commond = encoder_path + " -q 80 -resize " + str(new_size[0]) + " " + str(new_size[1]) + " " +infile + " -o "+output_path + str(index) + ".webp"
+  logging.debug(commond)
+  os.system(commond)
+
+
+def get_new_size(infile):
+  img = Image.open(infile)
+  width = img.size[0]
+  height = img.size[1]
+  phone_px = 500
+  scale = float(phone_px) / width
+  if width <= phone_px:
+    return width,height
+  else:
+    height = int(height * scale)
+  return phone_px, height
 
 def start():
-  index = 0
-  for infile in glob.glob("20140526190812/*.jpg"):
-    t = threading.Thread(target=create_image, args=(infile, index,))
+  index = 1
+  input_path = "C:/Test01/20150806205033/*.jpg"
+  for infile in glob(input_path):
+    print infile
+    t = Thread(target=convert_img_type, args=(infile, index,))
     t.start()
     t.join()
     index += 1
--- z2/z2/spiders/z2Spider.py
+++ z2/z2/spiders/z2Spider.py
@@ -38,8 +38,8 @@ class Spider(CrawlSpider):
     # )
 
     def start_requests(self):
-        for i in range(141):
-            yield Request(url='http://www.umei.cc/p/gaoqing/rihan/%s.htm'%(str(i)),
+        for i in range(1,22):
+            yield Request(url='http://www.umei.cc/p/gaoqing/cn/%s.htm'%(str(i)),
                           callback=self.parse_z2_key)
 
     def parse_z2_key(self, response):
